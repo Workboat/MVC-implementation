24376064d636f6fa7a5ef047123c3acb4c89ef97
root <root@vps.server.local>
2020-06-05 14:41:46 +0000

    Remove settings and vendor from the repository

--- settings/env.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_ENV["USE_PROD"]             = 'true';
-$_ENV["SANDBOX_ACCESS_TOKEN"] = '';
-$_ENV["SANDBOX_APP_ID"]       = '';
-$_ENV["SANDBOX_LOCATION_ID"]  = '';
-
--- settings/params.php
+++
@@ -1,5 +0,0 @@
-<?php
-$_PARAMS['price']        = 6;
-$_PARAMS['installation'] = 3;
-$_PARAMS['shipping']     = 3;
-$_PARAMS['tax']          = 1.13;
