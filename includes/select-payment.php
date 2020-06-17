d60b013b80917550c23d0e37b9c36bbe12a865bd
Workboat <65305965+Workboat@users.noreply.github.com>
2020-06-17 17:19:09 +0300

    Delete select-payment.php

--- includes/select-payment.php
+++
@@ -1,8 +0,0 @@
-<?php
-// We are working in WallpaperParser scope
-
-// Loading shipping form
-$form = $this->htmlLoader('select-payment');
-$html->find('#allWrapper')[0]->innertext = $form;
-$html = $this->injectJs($html);
-$html = $this->injectCss($html);
